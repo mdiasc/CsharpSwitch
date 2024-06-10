4a5f20bcd4aa8fc4ffa0ed2f038f3aec278e09c4
milena <milena-.dias@live.com>
2024-06-10 12:53:55 -0300

    If Else

---
+++ exercicio02/Exercicio02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Exercicio01
+{
+    public static void Main(string[] args)
+    {
+        int day = 4;
+        switch (day)
+        {
+            case 6:
+                Console.WriteLine("Today is Saturday.");
+                break;
+            case 7:
+                Console.WriteLine("Today is Sunday.");
+                break;
+            default:
+                Console.WriteLine("Looking forward to the Weekend.");
+                break;
+        }
+    }
+}
